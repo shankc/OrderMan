ed88190623b724d51ff56c7c7f9b7004af3839f1
Mayuukh Varshney <mayuukhvarshney@gmail.com>
2016-05-31 18:16:34 +0530

    First Commit

---
+++ app/src/main/java/com/kaidoh/mayuukhvarshney/orderman/MenuListDetailFragment.java
@@ -0,0 +1,166 @@
+package com.kaidoh.mayuukhvarshney.orderman;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.GridView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.TableLayout.LayoutParams;
+import com.kaidoh.mayuukhvarshney.orderman.dummy.DummyContent;
+/**
+ * A fragment representing a single MenuList detail screen.
+ * This fragment is either contained in a {@link MenuListListActivity}
+ * in two-pane mode (on tablets) or a {@link MenuListDetailActivity}
+ * on handsets.
+ */
+public class MenuListDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static String[] CONTENT={"Dosa","Idly","Sambar","Masala Dosa"};
+    public static int[] ICONS={R.mipmap.black_image,R.mipmap.black_image,R.mipmap.black_image,R.mipmap.black_image};
+    private ImageAdapter adapter;
+    GridView menuGrid;
+    protected int mPhotoSize, mPhotoSpacing;
+    public static final String ARG_ITEM_ID = "item_id";
+    TableLayout table;
+
+    TableRow tr_head;
+    TextView label_name,label_intime,label_outtime;
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public MenuListDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+
+            Activity activity = this.getActivity();
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.menu_and_order, container, false);
+
+        // Show the dummy content as text in a TextView.
+        mPhotoSize = getResources().getDimensionPixelSize(R.dimen.photo_size);
+        mPhotoSpacing = getResources().getDimensionPixelSize(R.dimen.photo_spacing);
+        adapter= new ImageAdapter(getActivity(),ICONS,CONTENT);
+        menuGrid= (GridView) rootView.findViewById(R.id.grid_view1);
+        menuGrid.setAdapter(adapter);
+        menuGrid.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (adapter.getNumColumns() == 0) {
+                    final int numColumns = (int) Math.floor(menuGrid.getWidth() / (mPhotoSize + mPhotoSpacing));
+                    if (numColumns > 0) {
+                        final int columnWidth = (menuGrid.getWidth() / numColumns);
+                        adapter.setNumColumns(numColumns);
+                        adapter.setItemHeight(columnWidth);
+
+
+                    }
+                }
+            }
+        });
+        table = (TableLayout) rootView.findViewById(R.id.main_table);
+        //  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        tr_head = new TableRow(getActivity());
+
+        tr_head.setId(R.id.worker_table);
+        tr_head.setBackgroundColor(Color.parseColor("#4D7AF9"));
+        tr_head.setLayoutParams(new LayoutParams(
+                LayoutParams.MATCH_PARENT,
+                LayoutParams.MATCH_PARENT));
+
+        label_name = new TextView(getActivity());
+        label_name.setId(R.id.worker_name);
+        label_name.setText("Quantity");
+        label_name.setTextSize(12);
+        label_name.setTextColor(Color.BLACK);
+        label_name.setPadding(15, 15, 15, 15);
+        tr_head.addView(label_name);// add the column to the table row here
+
+        label_intime = new TextView(getActivity());
+        label_intime.setId(R.id.worket_out);// define id that must be unique
+        label_intime.setText("Item-Name");
+         label_intime.setTextSize(12);
+        label_intime.setTextColor(Color.BLACK); // set the color
+        label_intime.setPadding(15, 15, 5, 15); // set the padding (if required)
+        tr_head.addView(label_intime);
+
+        table.addView(tr_head, new LayoutParams(
+                LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT));
+
+
+        int count =0;
+        for(int i=0;i<10;i++)
+        {
+
+            String Name=Integer.toString(i);
+            String Intime = Integer.toString(i+10);
+            String OutTime=Integer.toString(i + 1000);
+// Create the table row
+            TableRow tr = new TableRow(getActivity());
+            tr.setId(100 + count);
+            tr.setLayoutParams(new LayoutParams(
+                    LayoutParams.WRAP_CONTENT,
+                    LayoutParams.WRAP_CONTENT));
+
+            TextView names = new TextView(getActivity());
+            names.setId(200 + count);
+            names.setText(Name);
+            names.setPadding(55,45,45,45);
+            names.setTextColor(Color.BLACK);
+            tr.addView(names);
+
+            TextView in_time= new TextView(getActivity());
+            in_time.setId(200 + count);
+            in_time.setText((Intime));
+            in_time.setPadding(55,45,45,45);
+            in_time.setTextColor(Color.BLACK);
+            tr.addView(in_time);
+
+            table.addView(tr, new TableLayout.LayoutParams(
+                    LayoutParams.WRAP_CONTENT,
+                    LayoutParams.WRAP_CONTENT));
+            count++;
+        }
+
+
+        //if (mItem != null) {
+          //  ((TextView) rootView.findViewById(R.id.menulist_detail)).setText(mItem.details);
+        //.
+    //}
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/kaidoh/mayuukhvarshney/orderman/MenuListListActivity.java
@@ -0,0 +1,123 @@
+package com.kaidoh.mayuukhvarshney.orderman;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.kaidoh.mayuukhvarshney.orderman.dummy.DummyContent;
+
+import java.util.List;
+
+public class MenuListListActivity extends AppCompatActivity {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menulist_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
+
+
+
+        View recyclerView = findViewById(R.id.menulist_list);
+        assert recyclerView != null;
+        setupRecyclerView((RecyclerView) recyclerView);
+
+        if (findViewById(R.id.menulist_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+        }
+    }
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(DummyContent.ITEMS));
+    }
+
+    public class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final List<DummyContent.DummyItem> mValues;
+
+        public SimpleItemRecyclerViewAdapter(List<DummyContent.DummyItem> items) {
+            mValues = items;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.menulist_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mItem = mValues.get(position);
+            holder.mIdView.setText(mValues.get(position).id);
+            holder.mContentView.setText(mValues.get(position).content);
+
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mTwoPane) {
+                        Bundle arguments = new Bundle();
+                        arguments.putString(MenuListDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+                        MenuListDetailFragment fragment = new MenuListDetailFragment();
+                        fragment.setArguments(arguments);
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.menulist_detail_container, fragment)
+                                .commit();
+                    } else {
+                        Context context = v.getContext();
+                        Intent intent = new Intent(context, MenuListDetailActivity.class);
+                        intent.putExtra(MenuListDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+
+                        context.startActivity(intent);
+                    }
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final TextView mIdView;
+            public final TextView mContentView;
+            public DummyContent.DummyItem mItem;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mIdView = (TextView) view.findViewById(R.id.id);
+                mContentView = (TextView) view.findViewById(R.id.content);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString() + " '" + mContentView.getText() + "'";
+            }
+        }
+    }
+}
